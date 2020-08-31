d63a623c95c13378d8e0d772d0f0db8abbf5d3b4
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-08-31 11:29:11 +0200

    Update Drion.sh

--- Drion.sh
+++ Drion.sh
@@ -1,3 +1,7 @@
+# Mise à jour
+apt update
+apt-y full-upgrade
+
 # Install prereqs
 apt install -y bc build-essential dkms rsync raspberrypi-kernel-headers
 
@@ -18,7 +22,7 @@ dkms install -m rtl88x2bu -v ${VER}
 echo 88x2bu >> /etc/modules
 
 #Création d'une varible pour récupérer l'ID de la carte WI-FI:
-ID=$(ip a | grep '4:' |cut -d ' ' -f2 | cut -d ':' -f1)
+ID=$(ip a | grep '4:' | cut -d ' ' -f2 | cut -d ':' -f1)
 
 #Create a variable for change IP:
 COUNTER=1
