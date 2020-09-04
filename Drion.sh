d448292bbc75ea9fb5ed20f658a42dcc92f86d6e
FlorianTorchy <69973133+FlorianTorchy@users.noreply.github.com>
2020-09-04 09:42:50 +0200

    Update Drion.sh
    Modifications script avant test

--- Drion.sh
+++ Drion.sh
@@ -1,5 +1,5 @@
 # Install prereqs
-#apt install -y bc build-essential dkms rsync raspberrypi-kernel-headers
+apt install -y bc build-essential dkms rsync raspberrypi-kernel-headers
 
 # Download from a driver source 
 git clone https://github.com/cilynx/rtl88x2bu
@@ -20,20 +20,6 @@ echo rtl88x2bu >> /etc/modules
 #Création d'une varible pour récupérer l'ID de la carte WI-FI:
 ID=$(ip a | grep '4:' | cut -d ' ' -f2 | cut -d ':' -f1)
 
-#Create a variable for change IP:
-COUNTER=1
-NETWORK=10.1.6
-while [ $COUNTER -lt 254 ]
-do
-   if ping -c1 -w3 $NETWORK.$COUNTER >/dev/null 2>&1
-   then
-   COUNTER=$(( $COUNTER + 1 ))
-   else
-   IP=$NETWORK.$COUNTER
-   COUNTER=254
-   fi
-done
-
 #Création d'une variable pour l'ancienne adresse IP
 IPBASE=$(ip a | grep '10.1.6' | cut -d' ' -f6 | cut -d'/' -f1)
 
@@ -46,12 +32,9 @@ echo address $IPBASE >> /etc/network/interfaces
 echo netmask 255.255.255.0 >> /etc/network/interfaces
 echo gateway 10.1.6.1 >> /etc/network/interfaces
 echo auto $ID >> /etc/network/interfaces
-echo iface $ID inet static >> /etc/network/interfaces
+echo iface $ID inet dhcp >> /etc/network/interfaces
 echo  wpa-ssid TSSRARIEN >> /etc/network/interfaces
 echo  wpa-psk P455Support >> /etc/network/interfaces
-echo	address $IP >> /etc/network/interfaces
-echo	netmask 255.255.255.0 >> /etc/network/interfaces
-echo	gateway 10.1.6.1 >> /etc/network/interfaces
 
 #restart netwoking.service:
 systemctl restart networking.service
