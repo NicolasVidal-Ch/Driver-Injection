929126dbe3920e8415d86ee15c0298ddce7ec17c
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-11 14:34:31 +0200

    Update Drion.sh

--- Drion.sh
+++ Drion.sh
@@ -17,9 +17,6 @@ dkms build -m rtl88x2bu -v ${VER}
 dkms install -m rtl88x2bu -v ${VER}
 echo 88x2bu >> /etc/modules
 
-#Création d'une varible pour récupérer l'ID de la carte WI-FI:
-#ID=$(ip a | grep '4:' | cut -d ' ' -f2 | cut -d ':' -f1)
-
 #Old IP
 IPRASP=$(ip a | grep '10.1.6' | cut -d ' ' -f6 | cut -d '/' -f1)
 
